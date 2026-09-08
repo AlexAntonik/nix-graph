04f8450cfc8a58ce9f1f4de709878c7e81719e5b
AlexAntonik <antonikavv@gmail.com>
2026-09-08 20:43:37 +0300

    add formatter
    Signed-off-by: AlexAntonik <antonikavv@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,15 +3,20 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       name = "nix-graph";
       version = "0.0.1";
-      systems = [ "x86_64-linux" "aarch64-linux" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
-      packages = forAllSystems (system:
+      packages = forAllSystems (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
           nix-graph = pkgs.buildGoModule {
@@ -21,7 +26,12 @@
             src = self;
             vendorHash = null;
             doCheck = true;
-            ldflags = [ "-s" "-w" "-X main.version=${version}" ];
+            env.CGO_ENABLED = "0";
+            ldflags = [
+              "-s"
+              "-w"
+              "-X main.version=${version}"
+            ];
 
             nativeBuildInputs = [ pkgs.makeWrapper ];
             postInstall = ''
@@ -39,7 +49,8 @@
         {
           inherit nix-graph;
           default = nix-graph;
-        });
+        }
+      );
 
       apps = forAllSystems (system: {
         default = {
@@ -50,9 +61,16 @@
 
       devShells = forAllSystems (system: {
         default = nixpkgs.legacyPackages.${system}.mkShell {
-          packages = with nixpkgs.legacyPackages.${system}; [ go gopls nix ];
+          packages = with nixpkgs.legacyPackages.${system}; [
+            go
+            gopls
+            nix
+            nixfmt
+          ];
         };
       });
+
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
 
       checks = self.packages;
     };
